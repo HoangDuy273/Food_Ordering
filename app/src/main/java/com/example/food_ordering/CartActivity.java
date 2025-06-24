d263fa3136b21df8923c8548115a484a1114fd55
vinhnm203 <vinhnm203@gmail.com>
2025-06-24 08:26:39 +0700

    Cart
    Layout Cart Screen

---
+++ app/src/main/java/com/example/food_ordering/CartActivity.java
@@ -0,0 +1,13 @@
+package com.example.food_ordering;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CartActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedIntanceState){
+        super.onCreate(savedIntanceState);
+        setContentView(R.layout.activity_cart);
+    }
+}
